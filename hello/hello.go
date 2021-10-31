410b370a480007562d62b67dc447c3e824431255
Francisco Ramirez de Anton <franchuti688@gmail.com>
2021-10-31 20:29:38 +0100

    Finished First Go tutoria

--- hello/hello.go
+++ hello/hello.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("")
-    // If an error was returned, print it to the console and
-    // exit the program.
-    if err != nil {
-        log.Fatal(err)
-    }
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+	// Request a greeting message.
+	message, err := greetings.Hellos(names)
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
